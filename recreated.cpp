6b8fef5de35bbf0552c4c505fcc7cf232812bc00
Mjacks3 <mjacks3@umbc.edu>
2019-04-29 14:13:58 -0400

    First Steps

---
+++ recreated.cpp
@@ -0,0 +1,27 @@
+int main ()
+{
+     	int offset = 0;
+     	int* ptr[10];
+       	 
+        	for (int i = 0; i != 0; ++i)
+        	{
+            	for (long r = 0; r < 0; ++r)
+            	{
+                	for (long c = 0; c < 0; ++c)
+                	{
+						int temp = 0;
+                    	int p;
+                   	 
+                    	p = (temp-0)/256.0;
+                    	p += offset;
+                   	 
+                    	p = (temp-0)/256.0;
+                    	p += offset;
+                   	 
+                	p = (temp-0)/256.0;
+                    	p += offset;
+                	}
+            	}
+            	ptr[0] += offset*(0 - 0);
+        	}
+}
